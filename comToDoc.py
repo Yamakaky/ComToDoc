68a5c126415b5ec3e2fbccf67467106ba3775985
Yamakaky <yamakaky@gmail.com>
2012-12-29 12:59:47 +0100

    Ajout de la fonction de recuperation des fichiers
    Cette fonction prend en paramettre un dossier et renvoit une liste de tous les .dasm des sous-repertoires.

--- comToDoc.py
+++ comToDoc.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*-coding:Utf-8 -*
+import sys
+import os.path
+import glob
+
+def getFiles(dir):
+    """ Liste <dir> et renvoie la liste des .dasm qu'il contient (récursif) """
+    liste = list()    # contient la liste les fichiers .dasm
+    for file in glob.glob(dir + "/*"):
+        if os.path.isdir(file):
+            liste += getFiles(file)
+        elif os.path.splitext(file)[-1] == ".dasm":
+            liste.append(file)
+    return liste
+
+
+rootDir = sys.argv[1]
+listeFiles = getFiles(rootDir)
+print(listeFiles)

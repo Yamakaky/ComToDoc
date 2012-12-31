ba0946d02ed2686218c9635127fcdf017d89eec8
Yamakaky <yamakaky@gmail.com>
2012-12-31 09:53:03 +0100

    Encapsulation de main

--- comToDoc.py
+++ comToDoc.py
@@ -8,45 +8,50 @@ from comGetter import *
 def printHelp():
     print("help !")
 
+def main():
+    if len(sys.argv) < 2:
+        print("argument manquant")
+        return 1
 
-if len(sys.argv) < 2:
-    print("argument manquant")
-    sys.exit(1)
+    arg = sys.argv[1]
 
-arg = sys.argv[1]
+    if arg == "--help" or arg == "-h":
+        printHelp()
+        return 0
+    elif not os.path.exists(arg):
+        print("Dossier en argument introuvable")
+        return 1
 
-if arg == "--help" or arg == "-h":
-    printHelp()
-    sys.exit(0)
-elif not os.path.exists(arg):
-    print("Dossier en argument introuvable")
-    sys.exit(1)
-    
 
-rootDir = sys.argv[1]
-print("Création de la liste des fichiers...")
-listeFiles = getFiles(rootDir)
-print("Lecture des fichiers...")
-commentaires = getComs(listeFiles)
-print("Créaction des objets...")
-listeFonctions = Fonction.match(commentaires)
+    rootDir = sys.argv[1]
+    print("Création de la liste des fichiers...")
+    listeFiles = getFiles(rootDir)
+    print("Lecture des fichiers...")
+    commentaires = getComs(listeFiles)
+    print("Créaction des objets...")
+    listeFonctions = Fonction.match(commentaires)
 
-print("Écriture du fichier...")
-if os.path.exists("test.html"):
-    os.remove("test.html")
-    
-with open("test.html", "a") as file:
-    html = "<!DOCTYPE HTML>"
-    html += """<html>
-               <head>
-                 <meta charset=\"utf-8\" />
-                 <title>Documentation de FrOSt</title>
-               </head>
-               <body>"""
-    for fonction in listeFonctions:
-        html += fonction.getHTML()
-        
-    html += "</body></html>"
-    file.write(html)
+    print("Écriture du fichier...")
+    if os.path.exists("test.html"):
+        os.remove("test.html")
 
-print("Fichier généré !")
+    with open("test.html", "a") as file:
+        html = "<!DOCTYPE HTML>"
+        html += """<html>
+                   <head>
+                     <meta charset=\"utf-8\" />
+                     <title>Documentation de FrOSt</title>
+                   </head>
+                   <body>"""
+        for fonction in listeFonctions:
+            html += fonction.getHTML()
+
+        html += "</body></html>"
+        file.write(html)
+
+    print("Fichier généré !")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
